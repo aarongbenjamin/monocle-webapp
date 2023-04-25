94af06922ca34c77de113d10814f9d5375889508
Aaron Benjamin <aarongbenjamin@gmail.com>
2023-04-25 08:20:56 -0500

    adding initial claims post endpoint

--- server/routes/claims.ts
+++ server/routes/claims.ts
@@ -1,11 +1,30 @@
 import { Router } from 'express'
+import { Schema, model } from 'mongoose';
 
 const routes = Router();
+interface IClaim {
+    title: string,
+    incidentDate: Date,
+    createdDate: Date
+}
+const claimSchema = new Schema<IClaim>({
+    title: { type: String, required: true },
+    incidentDate: { type: Date, required: true },
+    createdDate: { type: Date, required: true }
+});
 
-routes.get('/claims', (req, res) => {
-    res.json({
-        what: "what?"
-    })
-})
+const Claim = model('Claim', claimSchema);
+
+routes.post('/claims', async (req, res) => {
+    const { title, incidentDate } = req.body;
+
+    const claim = new Claim({
+        title, incidentDate, createdDate: Date.now()
+    });
+
+    await claim.save();
+
+    res.json(claim.toJSON());
+});
 
 export default routes;
--- server/server.ts
+++ server/server.ts
@@ -2,17 +2,22 @@ import express from 'express'
 import cors from 'cors';
 import dotenv from 'dotenv'
 import claimRoutes from './routes/claims'
-dotenv.config({ path: './config.env'});
+import { connect } from 'mongoose';
+dotenv.config({ path: './config.env' });
 
 const app: express.Application = express();
 
 const port: number = parseInt(process.env.PORT ?? "5000");
+const mongoUrl = process.env.MONGO_URI ?? 'mongodb://localhost:27017';
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', claimRoutes);
 
 // Server setup
-app.listen(port, () => {
-    console.log(`TypeScript with Express http://localhost:${port}/`);
+app.listen(port, async () => {
+    await connect(mongoUrl, {
+        dbName: 'claims-management'
+    });
+    console.log("Successfully connected to MongoDB.");
 });
