4b741f1e00ed0080f73667409f8e45f777d5de75
Aaron Benjamin <aarongbenjamin@gmail.com>
2023-05-01 19:57:37 -0500

    file rename

--- client/src/Components/StartClaimModal/StartClaimModal.tsx
+++ client/src/Components/StartClaimModal/StartClaimModal.tsx
@@ -30,7 +30,7 @@ const StartClaimModal: FunctionComponent<StartClaimModalProps> = ({
     >
       <Box sx={style}>
         <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
+          This is where the initial inputs will be to start a claim/s
         </Typography>
         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
           Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
