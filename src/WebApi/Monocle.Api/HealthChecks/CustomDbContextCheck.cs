0844c4f6d6c3262ddd1afcfb29f0cd4e8aff725d
Aaron Benjamin <aarongbenjamin@gmail.com>
2023-09-14 16:21:47 -0500

    changing the CustomDbContextCheck

--- src/WebApi/Monocle.Api/HealthChecks/CustomDbContextCheck.cs
+++ src/WebApi/Monocle.Api/HealthChecks/CustomDbContextCheck.cs
@@ -9,31 +9,32 @@ public static class CustomDbContextCheckExtension
     public static IHealthChecksBuilder AddCustomDBContextCheck<TDbContext>(this IHealthChecksBuilder builder)
     where TDbContext : MonocleDbContext
     {
-        builder.AddDbContextCheck<TDbContext>(customTestQuery: async (context, cancellationToken) =>
+        builder.AddDbContextCheck(customTestQuery: (Func<TDbContext, CancellationToken, Task<bool>>?)(async (context, cancellationToken) =>
         {
             var logger = context.GetService<ILoggerFactory>().CreateLogger("MonocleDbContext-HealthCheck");
             logger.LogInformation("Checking db connection");
 
-            if (await context.Database.CanConnectAsync(cancellationToken))
-            {
-                return true;
-            }
+            return await context.Database.CanConnectAsync(cancellationToken) || await TryOpenConnectionAndLogError(context, logger, cancellationToken);
 
-            try
-            {
-                await context.Database.OpenConnectionAsync(cancellationToken: cancellationToken);
-            }
-            catch (System.Exception ex)
-            {
-                logger.LogError(ex, "An error occured when trying to connect to db");
-                return false;
-            }
-            finally
-            {
-                await context.Database.CloseConnectionAsync();
-            }
-            return true;
-        });
+        }));
         return builder;
+    }
+
+    private static async Task<bool> TryOpenConnectionAndLogError<TDbContext>(TDbContext context, ILogger logger, CancellationToken cancellationToken) where TDbContext : MonocleDbContext
+    {
+        try
+        {
+            await context.Database.OpenConnectionAsync(cancellationToken: cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occured when trying to open a connetion to {provider}", context.Database.ProviderName);
+            return false;
+        }
+        finally
+        {
+            await context.Database.CloseConnectionAsync();
+        }
+        return true;
     }
 }
