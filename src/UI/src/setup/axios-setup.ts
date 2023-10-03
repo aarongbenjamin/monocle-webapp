e8c11384de110b6808cab7da65c175bc9ebdabb7
Aaron Benjamin <aarongbenjamin@gmail.com>
2023-10-02 21:45:26 -0500

    fixed .env.development

--- src/UI/src/setup/axios-setup.ts
+++ src/UI/src/setup/axios-setup.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export default function setupAxios() {
-  if (process.env.NODE_ENV === 'development') {
-    axios.defaults.baseURL = 'http://localhost:5219';
-  } else {
-    const baseUrl = process.env.REACT_APP_API_BASE_URL;
-    axios.defaults.baseURL = baseUrl;
-  }
+  const baseUrl = process.env.REACT_APP_API_BASE_URL;
+  axios.defaults.baseURL = baseUrl;
 }
